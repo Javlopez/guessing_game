6f738aa1711cbe1a3aebbb5cde1ff617d9f4d729
Javier Lopez Lopez <sjavierlopez@gmail.com>
2024-10-29 19:35:45 -0600

    Adding loop

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,29 @@ fn main() {
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Plese type a number");
-
-    println!("You guessed {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
-    }
 
     println!("the secret number is: {}", secret_number);
+
+    loop {
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+        println!("You guessed {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    
+    }
+
 
 }
 
